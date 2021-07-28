f8a4203d1c429b82f5f30adbf667b8558798106d
Damian <pokemonmew34567@gmail.com>
2021-07-28 13:43:57 -0700

    Project End

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-let productArr = [];
-let productSelectors = document.getElementsByClassName("array-button-selector");
+const buttons = document.querySelectorAll('.array-button-selector');
+const itemNames = document.querySelectorAll('product-name');
+const itemPrices = document.querySelectorAll('product-price');
+const items = document.querySelectorAll('.product-card');
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        const filter = e.target.innerText;
+        items.forEach(item => {
+            const children = item.childNodes;
+            //console.log(children[3].childNodes[1]);
+            
+            if(children[3].childNodes[1].textContent.includes(`${filter}`))
+            {
+                console.log(`${filter}`);
+                item.style.display = 'block';
+            }
+            else if(filter === "All")
+            {
+                item.style.display = 'block';
 
-productSelectors.forEach(element => {
-    console.log(element);
+            }
+            else
+            {
+                item.style.display = 'none';
+            }
+            
+            
+        })
+
+    });
 });
+
+//children[3].childNodes[1].textContent.includes(`${filter}`)
